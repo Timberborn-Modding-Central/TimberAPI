2e444e63555071b9d6ed23ac2ada51300034fde1
Hannu Hytönen <hannu_94@hotmail.com>
2022-04-08 17:36:17 +0300

    Improved comment on EntityLink

--- TimberAPI/EntityLinkerSystem/EntityLink.cs
+++ TimberAPI/EntityLinkerSystem/EntityLink.cs
@@ -1,7 +1,9 @@
 ﻿namespace TimberbornAPI.EntityLinkerSystem
 {
     /// <summary>
-    /// Represent a link between two entities
+    /// Represent a link between two entities. A Link is used when two entities should
+    /// be connected to eachother somehow. For example to guide the behaviour of the other 
+    /// based on the stats of the other. 
     /// </summary>
     public class EntityLink
     {
