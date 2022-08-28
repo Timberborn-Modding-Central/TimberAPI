d8363e90e1fc158dad76d50bbd92e7b7d98e2000
KYPremco <KYPremco@gmail.com>
2022-08-28 10:48:06 +0200

    Fixed wrong exception message

--- Core/TimberApi.Internal/AssetSystem/Exceptions/ModAssetNotLoadedException.cs
+++ Core/TimberApi.Internal/AssetSystem/Exceptions/ModAssetNotLoadedException.cs
@@ -4,7 +4,7 @@ namespace TimberApi.Internal.AssetSystem.Exceptions
 {
     public class ModAssetNotLoadedException : Exception
     {
-        public ModAssetNotLoadedException(string assetBundle) : base($"AssetBundle ({assetBundle}) is already unloaded.")
+        public ModAssetNotLoadedException(string asset) : base($"Asset ({asset}) it not loaded")
         {
         }
     }
