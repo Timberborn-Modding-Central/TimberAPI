059b7921d14cd962e19d8408cb36bf7d4e41a5a9
KYPremco <KYPremco@gmail.com>
2022-09-01 10:55:35 +0200

    Forgot to push singleton runner

---
+++ Core/TimberApi.Internal/SingletonSystem/SingletonRunner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TimberApi.Internal.SingletonSystem.Singletons;
+
+namespace TimberApi.Internal.SingletonSystem
+{
+    public class SingletonRunner
+    {
+        private readonly ISingletonRepository _singletonRepository;
+
+        public SingletonRunner(ISingletonRepository singletonRepository)
+        {
+            _singletonRepository = singletonRepository;
+            Run();
+        }
+
+        private void Run()
+        {
+            SingletonActivator(_singletonRepository.GetSingletons<ITimberApiSeeder>(), singleton => singleton.Run());
+            SingletonActivator(_singletonRepository.GetSingletons<ITimberApiLoadableSingleton>(), singleton => singleton.Load());
+            SingletonActivator(_singletonRepository.GetSingletons<ITimberApiPostLoadableSingleton>(), singleton => singleton.PostLoad());
+        }
+
+        private static void SingletonActivator<T>(IEnumerable<T> singletons, Action<T> action) where T : class
+        {
+            foreach (T singleton in singletons)
+            {
+                action(singleton);
+            }
+        }
+    }
+}
