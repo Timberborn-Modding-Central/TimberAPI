35189d7fbeb97dcabe31c87e9ec519fc7f081577
KYPremco <KYPremco@gmail.com>
2022-09-01 20:36:02 +0200

    Implemented specifications

---
+++ Core/TimberApi.Internal/DependencyContainer/DependencyContainerConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using Bindito.Core;
+using TimberApi.Core2.Common;
+using TimberApi.Core2.ConfiguratorSystem;
+
+namespace TimberApi.Internal.DependencyContainer
+{
+    [Configurator(SceneEntrypoint.All)]
+    public class DependencyContainerConfigurator : IConfigurator
+    {
+        public void Configure(IContainerDefinition containerDefinition)
+        {
+            containerDefinition.Bind<DependencyContainerSetter>().AsSingleton();
+        }
+    }
+}
