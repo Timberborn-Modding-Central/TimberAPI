44b9b8b1dd3e31a7e88b4cac933935e0c77a6cd5
KYPremco <KYPremco@gmail.com>
2022-09-10 19:19:21 +0200

    Debug version extension

---
+++ Core/TimberApi.Common/Extensions/VersionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace TimberApi.Common.Extensions
+{
+    /// <summary>
+    ///     Checks if it's a placeholder and change it to a valid version to check
+    /// </summary>
+    public static class VersionExtensions
+    {
+        public static string ConvertVersionPlaceholder(this string version)
+        {
+            return version.Contains("PLACEHOLDER") ? "0.0.0.0" : version;
+        }
+    }
+}
--- Core/TimberApi.New/VersionSystem/Version.cs
+++ Core/TimberApi.New/VersionSystem/Version.cs
@@ -2,23 +2,11 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace TimberApi.Common.VersionSystem
+namespace TimberApi.New.VersionSystem
 {
     public class Version : IComparable<Version>, IComparable
     {
-        private readonly int _major;
-
-        private readonly int _minor;
-
-        private readonly int _patch;
-
-        private readonly int _revision;
-
-        private readonly string _preRelease;
-
-        public bool IsPreRelease => !string.IsNullOrEmpty(_preRelease);
-
-        private static Regex _versionRegex = new(@"^
+        private static readonly Regex _versionRegex = new(@"^
             ([0-9]+)                                    # Major
             \.
             ([0-9]+)                                    # Minor
@@ -27,8 +15,17 @@ namespace TimberApi.Common.VersionSystem
             [\.]?                                       # Optional revision dot
             ([0-9]+)?                                   # Revision (optional)
             (?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?   # Pre-release
-            $",
-            RegexOptions.IgnorePatternWhitespace);
+            $", RegexOptions.IgnorePatternWhitespace);
+
+        private readonly int _major;
+
+        private readonly int _minor;
+
+        private readonly int _patch;
+
+        private readonly string _preRelease;
+
+        private readonly int _revision;
 
         public Version(int major, int minor, int patch, int revision = 0, string preRelease = "")
         {
@@ -37,6 +34,31 @@ namespace TimberApi.Common.VersionSystem
             _patch = patch;
             _revision = revision;
             _preRelease = preRelease;
+        }
+
+        public bool IsPreRelease => !string.IsNullOrEmpty(_preRelease);
+
+        public int CompareTo(object? obj)
+        {
+            return obj == null ? 1 : CompareTo(obj as Version ?? throw new ArgumentException("Object is not a Version"));
+        }
+
+        public int CompareTo(Version? other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            foreach (int compareResult in CompareVersions(other))
+            {
+                if (compareResult != 0)
+                {
+                    return compareResult;
+                }
+            }
+
+            return 0;
         }
 
         public static Version Parse(string version)
@@ -49,27 +71,8 @@ namespace TimberApi.Common.VersionSystem
 
             Match regexMatch = _versionRegex.Match(version);
 
-            return new Version(
-                int.Parse(regexMatch.Groups[1].Value),
-                int.Parse(regexMatch.Groups[2].Value),
-                int.Parse(regexMatch.Groups[3].Value),
-                regexMatch.Groups[4].Success ? int.Parse(regexMatch.Groups[4].Value) : 0,
-                regexMatch.Groups[5].Value
-            );
-        }
-
-        public int CompareTo(Version? other)
-        {
-            if (other == null) return 1;
-            foreach (int compareResult in CompareVersions(other))
-            {
-                if (compareResult != 0)
-                {
-                    return compareResult;
-                }
-            }
-
-            return 0;
+            return new Version(int.Parse(regexMatch.Groups[1].Value), int.Parse(regexMatch.Groups[2].Value), int.Parse(regexMatch.Groups[3].Value),
+                regexMatch.Groups[4].Success ? int.Parse(regexMatch.Groups[4].Value) : 0, regexMatch.Groups[5].Value);
         }
 
         public IEnumerable<int> CompareVersions(Version other)
@@ -83,11 +86,6 @@ namespace TimberApi.Common.VersionSystem
         public override string ToString()
         {
             return $"{_major}.{_minor}.{_patch}.{_revision}{(IsPreRelease ? "-" + _preRelease : "")}";
-        }
-
-        public int CompareTo(object? obj)
-        {
-            return obj == null ? 1 : CompareTo(obj as Version ?? throw new ArgumentException("Object is not a Version"));
         }
 
         public static bool operator >=(Version operand1, Version operand2)
