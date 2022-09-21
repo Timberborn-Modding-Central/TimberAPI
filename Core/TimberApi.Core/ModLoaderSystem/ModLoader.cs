78c04cd69fb6f7e6d3fe29736149d812d6f452f2
KYPremco <KYPremco@gmail.com>
2022-09-21 22:47:45 +0200

    Fixed IModEntrypoint crash when none existing

--- Core/TimberApi.Core/ModLoaderSystem/ModLoader.cs
+++ Core/TimberApi.Core/ModLoaderSystem/ModLoader.cs
@@ -187,7 +187,7 @@ namespace TimberApi.Core.ModLoaderSystem
             try
             {
                 IModEntrypoint? modEntrypoint = assembly.GetTypes().Where(type => type.GetInterfaces().Contains(typeof(IModEntrypoint)) && type.GetConstructor(Type.EmptyTypes) != null)
-                    .Select(type => Activator.CreateInstance(type) as IModEntrypoint).Single();
+                    .Select(type => Activator.CreateInstance(type) as IModEntrypoint).SingleOrDefault();
 
                 if (modEntrypoint == null)
                 {
