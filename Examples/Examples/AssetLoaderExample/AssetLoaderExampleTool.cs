79de5de15d63681030fc6ed5f6356919124ffe7b
hytonhan <hannuhytonen@gmail.com>
2022-10-19 21:59:29 +0300

    Updated Example project to work with v0TimberAPI 0.5

---
+++ Examples/Examples/AssetLoaderExample/AssetLoaderExampleTool.cs
@@ -0,0 +1,18 @@
+using Timberborn.ToolSystem;
+using static TimberAPIExample.TimberAPIExamplePlugin;
+
+namespace TimberAPIExample.Examples.AssetLoaderExample
+{
+    public class AssetLoaderExampleTool : Tool
+    {
+        public override void Enter()
+        {
+            Log.LogInfo("Selected");
+        }
+
+        public override void Exit()
+        {
+            Log.LogInfo("Deselected");
+        }
+    }
+}
