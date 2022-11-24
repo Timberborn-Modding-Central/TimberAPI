634bad1a40f2b4061d9fe1a13f256d319d7d4438
KYPremco <KYPremco@gmail.com>
2022-11-24 23:40:33 +0100

    Fix

--- Loaders/TimberApi.BepInEx.Plugin.Loader/TimberApiPluginEntrypoint.cs
+++ Loaders/TimberApi.BepInEx.Plugin.Loader/TimberApiPluginEntrypoint.cs
@@ -5,19 +5,31 @@ using Bindito.Core;
 using HarmonyLib;
 using TimberApi.Common.LoggingSystem;
 using Timberborn.Bootstrapper;
+using Timberborn.Core;
 
 namespace TimberApi.BepInExPlugin.Loader
 {
     [BepInPlugin("timberapi.bepinex.entrypoint", "TimberAPI BepInEx Entrypoint", "1.0.0")]
     public class TimberApiPluginEntrypoint : BaseUnityPlugin
     {
-        private static readonly string TimberApiRootPath = Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.Parent.ToString();
+        private static readonly string TimberApiRootPath =
+            Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.Parent.ToString();
 
         public void Awake()
         {
-            new Harmony("TimberApi.BepInExEntry").Patch(AccessTools.Method(typeof(BootstrapperConfigurator), "Configure"),
-                new HarmonyMethod(AccessTools.Method(typeof(TimberApiPluginEntrypoint), nameof(BootstrapperConfiguratorPatch))));
+            var harmony = new Harmony("TimberApi.BepInExEntry");
 
+            harmony.Patch(AccessTools.Method(typeof(BootstrapperConfigurator), "Configure"),
+                new HarmonyMethod(AccessTools.Method(typeof(TimberApiPluginEntrypoint),
+                    nameof(BootstrapperConfiguratorPatch))));
+
+            harmony.Patch(AccessTools.Method(typeof(GameStartLogger), "Log"),
+                new HarmonyMethod(
+                    AccessTools.Method(typeof(TimberApiPluginEntrypoint), nameof(GameStartLoggerLogPatch))));
+        }
+
+        public static void GameStartLoggerLogPatch()
+        {
             BepInExStartupLoader.Run(TimberApiRootPath);
         }
 
