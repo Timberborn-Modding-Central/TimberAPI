658b57063f195e796e4c3b7bda7a411a00a7c2be
KYPremco <KYPremco@gmail.com>
2023-01-13 21:03:03 +0100

    Improved version placeholder for developing

--- Core/TimberApi/Versions.cs
+++ Core/TimberApi/Versions.cs
@@ -7,8 +7,10 @@ namespace TimberApi
     {
         public static readonly Version GameVersion = Version.Parse(Timberborn.Versioning.Versions.VersionNumber);
 
-        public static Version TimberApiVersion { get; } = Version.Parse("TIMBER_API_VERSION_PLACEHOLDER".ReplacePlaceholderWithFakeVersion());
+        public static Version TimberApiVersion { get; } =
+            Version.Parse("TIMBER_API_VERSION_PLACEHOLDER".ReplacePlaceholderWithFakeVersion("1.0.0.0"));
 
-        internal static Version TimberApiMinimumGameVersion { get; } = Version.Parse("MINIMUN_GAME_VERSION_PLACEHOLDER".ReplacePlaceholderWithFakeVersion());
+        internal static Version TimberApiMinimumGameVersion { get; } =
+            Version.Parse("MINIMUN_GAME_VERSION_PLACEHOLDER".ReplacePlaceholderWithFakeVersion("0.0.0.0"));
     }
 }
