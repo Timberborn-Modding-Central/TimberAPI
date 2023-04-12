b821a3c0447b1ac92efeb286ba6943ba4e99f2e1
hytonhan <hannuhytonen@gmail.com>
2023-04-12 19:24:22 +0300

    Fixed dirt shader?

--- Core/TimberApi/ShaderSystem/ShaderAppliers/DirtUrpApplier.cs
+++ Core/TimberApi/ShaderSystem/ShaderAppliers/DirtUrpApplier.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 namespace TimberApi.ShaderSystem.ShaderAppliers
 {
@@ -8,7 +10,7 @@ namespace TimberApi.ShaderSystem.ShaderAppliers
 
         public void LoadShader()
         {
-            //_dirtUrpShader = Resources.Load<GameObject>("Buildings/Wellbeing/Carousel/CarouselConstructionSite5x5+3x1Dirt").GetComponentsInChildren<MeshRenderer>()[1].materials[0].shader;
+            _dirtUrpShader = Resources.Load<GameObject>("constructionsites/constructionsite5x5+3x1dirt/ConstructionSite5x5+3x1Dirt").GetComponentsInChildren<MeshRenderer>()[0].materials[0].shader;
         }
 
         public bool ShouldApplyShader(Material material)
@@ -17,7 +19,7 @@ namespace TimberApi.ShaderSystem.ShaderAppliers
         }
 
         public void Apply(Material material)
-        {
+        { 
             material.shader = _dirtUrpShader;
         }
     }
