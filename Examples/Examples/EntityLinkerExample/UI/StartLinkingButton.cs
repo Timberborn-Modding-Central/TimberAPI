dc90aa4a671136fce1efcdc1c5815410090813be
hytonhan <56257488+hytonhan@users.noreply.github.com>
2023-04-15 12:21:23 +0300

    Fixed PickObjectTool crash (#66)

--- Examples/Examples/EntityLinkerExample/UI/StartLinkingButton.cs
+++ Examples/Examples/EntityLinkerExample/UI/StartLinkingButton.cs
@@ -6,7 +6,7 @@ using Timberborn.SelectionSystem;
 using Timberborn.ToolSystem;
 using UnityEngine;
 using UnityEngine.UIElements;
-using TimberApi.ToolSystem;
+using TimberApi.ObjectSelectionSystem;
 
 namespace TimberAPIExample.Examples.EntityLinkerExample.UI
 {
