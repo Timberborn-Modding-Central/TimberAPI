2b384d2aa852a9c80a610f2d493180ef6700895c
KYPremco <33104651+KYPremco@users.noreply.github.com>
2023-04-20 22:36:57 +0200

    Fixed for Timberborn 0.4.1.1 (#68)

--- Core/TimberApi/ObjectSelectionSystem/PickObjectTool.cs
+++ Core/TimberApi/ObjectSelectionSystem/PickObjectTool.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Timberborn.Common;
+using Timberborn.BaseComponentSystem;
 using Timberborn.CoreUI;
 using Timberborn.EntitySystem;
 using Timberborn.InputSystem;
@@ -12,24 +10,35 @@ using UnityEngine;
 
 namespace TimberApi.ObjectSelectionSystem
 {
-
     // TODO: This class has commented code regarding custom cursor from PickObjectTool
     //       In the future the code can be uncommented, when the old cursor for the tool is recovered.
     public class PickObjectTool : Tool, IInputProcessor
     {
         //private static readonly string CursorKey = "PickObjectCursor";
+        
         private readonly InputService _inputService;
+        
         private readonly ToolManager _toolManager;
+        
         private readonly Highlighter _highlighter;
+        
         private readonly Colors _colors;
+        
         private readonly EntityComponentRegistry _entityComponentRegistry;
+        
         //private readonly CursorService _cursorService;
+        
         private readonly SelectableObjectRaycaster _selectableObjectRaycaster;
-        private ToolDescription _toolDescription;
-        private string _warning;
-        private readonly HashSet<GameObject> _allCandidates = new HashSet<GameObject>();
-        private Func<GameObject, string> _validateCandidate;
-        private Action<GameObject> _callback;
+        
+        private ToolDescription _toolDescription = null!;
+        
+        private string _warning = "";
+        
+        private readonly Dictionary<GameObject, BaseComponent> _allCandidates = new();
+        
+        private Func<GameObject, string> _validateCandidate = null!;
+        
+        private Action<GameObject> _callback = null!;
 
         public PickObjectTool(InputService inputService, ToolManager toolManager, Highlighter highlighter, Colors colors, EntityComponentRegistry entityComponentRegistry, CursorService cursorService, SelectableObjectRaycaster selectableObjectRaycaster)
         {
@@ -65,29 +74,31 @@ namespace TimberApi.ObjectSelectionSystem
             return _warning;
         }
 
-        public void StartPicking<T>(string title, string description, Func<GameObject, string> validateCandidate, Action<GameObject> callback) where T : MonoBehaviour, IRegisteredComponent
+        public void StartPicking<T>(string title, string description, Func<GameObject, string> validateCandidate, Action<GameObject> callback) where T : BaseComponent, IRegisteredComponent
         {
             _toolDescription = CreateDescription(title, description);
             _validateCandidate = validateCandidate;
             _callback = callback;
             _allCandidates.Clear();
-            IEnumerable<GameObject> values = from component in _entityComponentRegistry.GetEnabled<T>()
-                                             select component.gameObject;
-            _allCandidates.AddRange(values);
+            
+            foreach (var component in _entityComponentRegistry.GetEnabled<T>())
+            {
+                _allCandidates.Add(component.GameObjectFast, component);
+            }
             _toolManager.SwitchTool(this);
-        }
-
+        } 
+        
         public bool ProcessInput()
         {
             HighlightCandidates();
-            if (_selectableObjectRaycaster.TryHitSelectableObject(out var hitObject) && _allCandidates.Contains(hitObject))
+            if (_selectableObjectRaycaster.TryHitSelectableObject(out var hitObject) && _allCandidates.ContainsKey(hitObject.GameObjectFast))
             {
                 _highlighter.HighlightSecondary(hitObject, _colors.EntitySelection);
-                _warning = _validateCandidate(hitObject);
-                if (_inputService.SelectionStart && !_inputService.MouseOverUI)
+                _warning = _validateCandidate(hitObject.GameObjectFast);
+                if (_inputService is { SelectionStart: true, MouseOverUI: false })
                 {
                     _toolManager.SwitchToDefaultTool();
-                    _callback(hitObject);
+                    _callback(hitObject.GameObjectFast);
                     return true;
                 }
             }
@@ -98,16 +109,15 @@ namespace TimberApi.ObjectSelectionSystem
         private void HighlightCandidates()
         {
             _highlighter.UnhighlightAllSecondary();
-            foreach (GameObject allCandidate in _allCandidates)
+            foreach (var allCandidate in _allCandidates)
             {
-                Color color = ((_validateCandidate(allCandidate) == "") ? _colors.BuildablePreview : _colors.UnbuildablePreview);
-                _highlighter.HighlightSecondary(allCandidate, color);
+                var color = _validateCandidate(allCandidate.Key) == "" ? _colors.BuildablePreview : _colors.UnbuildablePreview; _highlighter.HighlightSecondary(allCandidate.Value, color);
             }
         }
 
         private static ToolDescription CreateDescription(string title, string description)
         {
-            ToolDescription.Builder builder = new ToolDescription.Builder(title);
+            var builder = new ToolDescription.Builder(title);
             builder.AddSection(description);
             return builder.Build();
         }
