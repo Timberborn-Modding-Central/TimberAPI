746e05252c265b8de7fcf8f71aaa0b8ab8d9e4ee
KYPremco <KYPremco@gmail.com>
2023-05-07 14:11:39 +0200

    Removed unused import

--- Core/TimberApi/TimberApi.cs
+++ Core/TimberApi/TimberApi.cs
@@ -3,7 +3,6 @@ using TimberApi.Common;
 using TimberApi.Common.ConsoleSystem;
 using TimberApi.ConfigSystem;
 using TimberApi.DependencyContainerSystem;
-using UnityEngine;
 
 namespace TimberApi
 {
