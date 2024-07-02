62bec0b40e285d79ec530c75103c0c0eb3e3dd89
KYPremco <KYPremco@gmail.com>
2024-07-02 09:27:54 +0200

    Things

---
+++ TimberApi/Test.cs
@@ -0,0 +1,13 @@
+using Timberborn.ModManagerScene;
+using UnityEngine;
+
+namespace TimberApi;
+
+public class Test : IModStarter
+{
+
+    public void StartMod()
+    {
+        Debug.LogError("SMOKING HOT");
+    }
+}
