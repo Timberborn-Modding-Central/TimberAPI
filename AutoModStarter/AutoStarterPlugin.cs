35585ae0280b11254fa6b257e4ac0c9638e8f06c
KYPremco <KYPremco@gmail.com>
2024-07-02 09:28:03 +0200

    Add auto starter bepinex plugin

---
+++ AutoModStarter/AutoStarterPlugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+using Timberborn.Modding;
+using Timberborn.ModdingAssets;
+using Timberborn.ModManagerScene;
+using Timberborn.ModManagerSceneUI;
+
+namespace AutoModStarter;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class AutoStarterPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(ModManagerScenePanelPatch));
+    }
+}
+
+class ModManagerScenePanelPatch
+{
+    [HarmonyPatch(typeof(ModManagerScenePanel), "Awake")]
+    [HarmonyPrefix]
+    static bool Prefix()
+    {
+        ModRepository modRepository = ModManagerScenePanel.CreateModRepository();
+        ModdedState.AddMods(modRepository.EnabledMods.Select<Mod, string>((Func<Mod, string>) (mod => mod.Manifest.Name)));
+        new ModCodeStarter(modRepository).Start();
+        new ModAssetBundleLoader(modRepository).Load();
+        ModManagerScenePanel.StartGame();
+
+        return false;
+    }
+}
+
