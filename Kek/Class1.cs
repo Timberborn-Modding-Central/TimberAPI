811d61cb7ca49e438eae6b58ab56f13d64815de2
KYPremco <KYPremco@gmail.com>
2024-08-07 17:21:30 +0200

    New testing classes

---
+++ Kek/Class1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Kek
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            // Console.WriteLine("AAAAAAA");
+            Debug.LogWarning("AAAAAAAAAAA");
+        }
+    }
+}
---
+++ RoslynCompiler/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RoslynCompiler
+{
+    public class Class1
+    {
+    }
+}
